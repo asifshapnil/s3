1006843ea5347ace50439d37862177e27e181d24
asif adham <asifshapnil@gmail.com>
2022-10-30 21:29:55 +0600

    logging

--- src/device/device.controller.ts
+++ src/device/device.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { DeviceDTO } from 'src/dto/device.dto';
 import { DeviceModel } from 'src/models/device.model';
 import { BaseController } from 'src/shared/base-controller';
@@ -6,13 +6,17 @@ import { DeviceService } from './device.service';
 
 @Controller('device')
 export class DeviceController extends BaseController {
+  logger: Logger;
+
   constructor(public deviceService: DeviceService) {
     super(deviceService);
+    this.logger = new Logger;
   }
 
   @Post()
   @UsePipes(new ValidationPipe())
   async postDevice(@Body() requestData: DeviceDTO): Promise<DeviceModel> {
+    this.logger.log('test log');
     try {
       return await this.deviceService.insert(requestData);      
     } catch (error) {
