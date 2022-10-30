5f3ffc96e94619bb4616121e4a0d37106a411e34
asif adham <asifshapnil@gmail.com>
2022-10-30 20:47:27 +0600

    handle exceptions

--- src/device/device.controller.ts
+++ src/device/device.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { DeviceDTO } from 'src/dto/device.dto';
 import { DeviceModel } from 'src/models/device.model';
 import { BaseController } from 'src/shared/base-controller';
@@ -13,6 +13,13 @@ export class DeviceController extends BaseController {
   @Post()
   @UsePipes(new ValidationPipe())
   async postDevice(@Body() requestData: DeviceDTO): Promise<DeviceModel> {
-    return await this.deviceService.insert(requestData);
+    try {
+      return await this.deviceService.insert(requestData);      
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'Something went wrong with the request',
+      }, HttpStatus.FORBIDDEN);
+    }
   }
 }
